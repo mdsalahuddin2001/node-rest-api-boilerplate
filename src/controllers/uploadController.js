815dfbda626b190eb3f9d3473c5aca77b32965fd
ferdous <hassanferdous101@gmail.com>
2023-08-02 14:00:20 +0600

    handle image uload to imagekit cloud

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,32 +1,35 @@
-const multer = require("multer");
-const { v2: cloudinary } = require("cloudinary");
-const streamifier = require("streamifier");
+const multer = require('multer');
+const streamifier = require('streamifier');
 
 const upload = multer();
+const ImageKit = require('imagekit');
 
 // @desc      Upload file
 // @route     POST /api/v1/upload
 // @access    Private
-exports.uploadFile = async (req, res) => {
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
-  const streamUpload = (req) => {
-    return new Promise((resolve, reject) => {
-      const stream = cloudinary.uploader.upload_stream((error, result) => {
-        if (result) {
-          resolve(result);
-        } else {
-          reject(error);
-        }
-      });
-      streamifier.createReadStream(req.file.buffer).pipe(stream);
-    });
-  };
-  const result = await streamUpload(req);
-  res.send(result);
+
+const imagekit = new ImageKit({
+	publicKey: process.env.IMAGEKIT_PUBLICK_KEY,
+	privateKey: process.env.IMAGEIT_PRIVATE_KEY,
+	urlEndpoint: process.env.IMAGEKIT_ENDPOINT,
+});
+
+exports.uploadFile = async (req, res, next) => {
+	imagekit
+		.upload({
+			file: req.file.buffer,
+			fileName: req.file.originalname,
+			useUniqueFileName: true,
+		})
+		.then((res) => {
+			req.body.image = res.url;
+			req.uploadedFile = res;
+			next();
+		})
+		.catch((err) => {
+			next(err);
+		});
 };
 
+exports.imagekit = imagekit;
 exports.upload = upload;
