5e372a49d17fb3354368bbb87465fd9aa4b720a8
mdsalahuddin2001 <mdsalahuddin46464@gmail.com>
2023-08-06 11:28:54 +0600

    upload file updated

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,34 +1,55 @@
-const multer = require('multer');
-const streamifier = require('streamifier');
+const multer = require("multer");
+const ImageKit = require("imagekit");
+const createError = require("http-errors");
+const { ALLOWED_FILE_TYPES, MAX_FILE_SIZE } = require("../config");
 
-const upload = multer();
-const ImageKit = require('imagekit');
+const fileFilter = (req, file, cb) => {
+  if (!file.mimetype.startsWith("image/")) {
+    return cb(createError(400, "Only image file are allowed."), false);
+  }
+  if (file.size > MAX_FILE_SIZE) {
+    return cb(createError(400, "File size is too big"), false);
+  }
+
+  if (!ALLOWED_FILE_TYPES.includes(file.mimetype)) {
+    return cb(
+      createError(400, "Only jpg,jpeg and png files are allowed."),
+      false
+    );
+  }
+  req.body.image = file.originalname;
+  cb(null, true);
+};
+
+const upload = multer({
+  fileFilter,
+});
 
 // @desc      Upload file
 // @route     POST /api/v1/upload
 // @access    Private
 
 const imagekit = new ImageKit({
-	publicKey: process.env.IMAGEKIT_PUBLICK_KEY,
-	privateKey: process.env.IMAGEIT_PRIVATE_KEY,
-	urlEndpoint: process.env.IMAGEKIT_ENDPOINT,
+  publicKey: process.env.IMAGEKIT_PUBLICK_KEY,
+  privateKey: process.env.IMAGEIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_ENDPOINT,
 });
 
 exports.uploadFile = async (req, res, next) => {
-	imagekit
-		.upload({
-			file: req.file.buffer,
-			fileName: req.file.originalname,
-			useUniqueFileName: true,
-		})
-		.then((res) => {
-			req.body.image = res.url;
-			req.uploadedFile = res;
-			next();
-		})
-		.catch((err) => {
-			next(err);
-		});
+  imagekit
+    .upload({
+      file: req.file.buffer,
+      fileName: req.file.originalname,
+      useUniqueFileName: true,
+    })
+    .then((res) => {
+      req.body.image = res.url;
+      req.uploadedFile = res;
+      next();
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 exports.imagekit = imagekit;
--- src/routers/productRoter.js
+++ src/routers/productRoter.js
@@ -1,28 +1,36 @@
-const express = require('express');
+const express = require("express");
 const {
-	getProducts,
-	getSingleProduct,
-	addProduct,
-	deleteProduct,
-	updateProduct,
-	reviewProduct,
-} = require('../controllers/productController');
-const validate = require('../middlewares/validate');
-const { productsValidation } = require('../validations');
-const { protect, authorize } = require('../middlewares/auth');
-const { upload, uploadFile } = require('../controllers/uploadController');
+  getProducts,
+  getSingleProduct,
+  addProduct,
+  deleteProduct,
+  updateProduct,
+  reviewProduct,
+} = require("../controllers/productController");
+const validate = require("../middlewares/validate");
+const { productsValidation } = require("../validations");
+const { protect, authorize } = require("../middlewares/auth");
+const { upload, uploadFile } = require("../controllers/uploadController");
 const router = express.Router();
 
 // router.post("/", createProduct);
 
 // admin & super admin
-router.route('/').get(getProducts).post(upload.single('file'), uploadFile, addProduct);
+router
+  .route("/")
+  .get(getProducts)
+  .post(
+    upload.single("image"),
+    validate(productsValidation.createProduct),
+    uploadFile,
+    addProduct
+  );
 
-router.route('/:id/reviews').post(protect, reviewProduct);
+router.route("/:id/reviews").post(protect, reviewProduct);
 
 router
-	.route('/:id')
-	.get(getSingleProduct)
-	.delete(protect, authorize('admin', 'super-admin'), deleteProduct)
-	.patch(protect, authorize('admin', 'super-admin'), updateProduct);
+  .route("/:id")
+  .get(getSingleProduct)
+  .delete(protect, authorize("admin", "super-admin"), deleteProduct)
+  .patch(protect, authorize("admin", "super-admin"), updateProduct);
 module.exports = router;
